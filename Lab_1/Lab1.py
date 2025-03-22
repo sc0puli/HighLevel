ebb5efe7f67d83eddb6a0c739c4b14e53f06df60
sc0puli <tamirb2002@mail.ru>
2025-03-22 16:11:33 +0300

    moved to class Gate

--- Lab_1/Lab1.py
+++ Lab_1/Lab1.py
@@ -1,7 +1,12 @@
 import re
 import numpy as np
 
+
 class Table:
+    '''
+    Класс для хранения NLDM таблицы
+    '''
+
     def __init__(self, _name, _tr_time, _output_cap):
         #create table after cell name
         self.name = _name
@@ -14,8 +19,8 @@ class Table:
         self.fall_tr = []
         self.rise_tr = []
 
+
 def lib_parser(path, name):
-    tables = []
     curr_table = ''
 
     flag_sit = 0
@@ -39,62 +44,44 @@ def lib_parser(path, name):
                 flag_coc = 0
             if flag_inner_cap:
                 # print(line)
-                for cell in tables:
-                    if cell.name == curr_table:
-                        cell.inner_cap = float(line)
-
+                curr_table.inner_cap = float(line)
                 flag_inner_cap = 0
 
             if flag_cf:
                 cf = []
-                for i in range(tables[0].tr_time.size):
+                for i in range(curr_table.tr_time.size):
                     cf.append(np.array(line.replace("\n", "").split(","), dtype='float'))
                     line = next(infile)
-
-                for cell in tables:
-                    if cell.name == curr_table:
-                        cell.cell_fall = np.vstack(cf)
-
+                curr_table.cell_fall = np.vstack(cf)
                 flag_cf = 0
 
             if flag_cr:
                 cr = []
-                for i in range(tables[0].tr_time.size):
+                for i in range(curr_table.tr_time.size):
                     cr.append(np.array(line.replace("\n", "").split(","), dtype='float'))
                     line = next(infile)
-
-                for cell in tables:
-                    if cell.name == curr_table:
-                        cell.cell_rise = np.vstack(cr)
-
+                curr_table.cell_rise = np.vstack(cr)
                 flag_cr = 0
 
             if flag_ft:
                 ft = []
-                for i in range(tables[0].tr_time.size):
+                for i in range(curr_table.tr_time.size):
                     ft.append(np.array(line.replace("\n", "").split(","), dtype='float'))
                     line = next(infile)
-
-                for cell in tables:
-                    if cell.name == curr_table:
-                        cell.fall_tr = np.vstack(ft)
-
+                curr_table.fall_tr = np.vstack(ft)
                 flag_ft = 0
 
             if flag_rt:
                 rt = []
-                for i in range(tables[0].tr_time.size):
+                for i in range(curr_table.tr_time.size):
                     rt.append(np.array(line.replace("\n", "").split(","), dtype='float'))
                     line = next(infile)
-
-                for cell in tables:
-                    if cell.name == curr_table:
-                        cell.rise_tr = np.vstack(rt)
-
+                curr_table.rise_tr = np.vstack(rt)
                 flag_rt = 0
+                break
 
             #flag catching
-            if re.match("string_input_transtion", line):
+            if re.match("string_input_transition", line):
                 flag_sit = 1
             elif re.match("column_output_capacitance", line):
                 flag_coc = 1
@@ -108,15 +95,13 @@ def lib_parser(path, name):
                 flag_ft = 1
             elif re.match("rise_transition", line):
                 flag_rt = 1
-            elif re.match('AND|NAND|NOR|OR|XOR|XNOR|BUF|INV', line):
-                tables.append(Table(line.replace('\n',''), sit_np, coc_np))
-                curr_table = line.replace('\n','')
+            elif re.match(name, line):
+                curr_table = Table(line.replace('\n', ''), sit_np, coc_np)
 
-    for table in tables:
-        if table.name == name:
-            return table
+    return curr_table
 
-def find_round(table: Table, t_rise: float, c_out: float, edge: str):
+
+def get_value_rounded(table: Table, t_rise: float, c_out: float, edge: str, to_get: str= 'delay'):
     found_time_exact, cap_found_exact = False, False
     t_index, c_index = 0, 0
     init_t_r, init_c_out = t_rise, c_out
@@ -148,56 +133,114 @@ def find_round(table: Table, t_rise: float, c_out: float, edge: str):
 
     # print(t_index, c_index)
 
-    if edge == 'posedge':
-        lut = table.cell_rise
-    elif edge == 'negedge':
-        lut = table.cell_fall
+    if to_get == 'delay':
+        if edge == 'posedge':
+            lut = table.cell_rise
+        elif edge == 'negedge':
+            lut = table.cell_fall
+        else:
+            raise ValueError("[ERROR] Wrong edge type!")
+    elif to_get == 'transition':
+        if edge == 'posedge':
+            lut = table.rise_tr
+        elif edge == 'negedge':
+            lut = table.fall_tr
+        else:
+            raise ValueError("[ERROR] Wrong edge type!")
     else:
-        print("[ERROR] Wrong edge type!")
-        return -1
+        raise ValueError("[ERROR] Wrong type!")
+    # if found_time_exact and cap_found_exact:
+    #     print(f'For element {table.name} and tr_inp {t_rise}, cap_out {c_out} was found exact tr_out {lut[t_index][c_index]}\n')
+    # else:
+    #     print(f"For element {table.name} was not found exact value!\ntr_inp {init_t_r} was rounded to {t_rise}\ncap_out {init_c_out} was rounded to {c_out}\nFor rounded values was found tr_out {lut[t_index][c_index]}\n")
 
-    if found_time_exact and cap_found_exact:
-        print(f'For element {table.name} and tr_inp {t_rise}, cap_out {c_out} was found exact tr_out {lut[t_index][c_index]}\n')
+    return lut[t_index][c_index]
+
+def get_value_interpolated(table: Table, t_rise: float, c_out: float, edge: str, to_get: str='delay'):
+    if to_get == 'delay':
+        if edge == 'posedge':
+            lut = table.cell_rise
+        elif edge == 'negedge':
+            lut = table.cell_fall
+        else:
+            raise ValueError("[ERROR] Wrong edge type!")
+    elif to_get == 'transition':
+        if edge == 'posedge':
+            lut = table.rise_tr
+        elif edge == 'negedge':
+            lut = table.fall_tr
+        else:
+            raise ValueError("[ERROR] Wrong edge type!")
     else:
-        print(f"For element {table.name} was not found exact value!\ntr_inp {init_t_r} was rounded to {t_rise}\ncap_out {init_c_out} was rounded to {c_out}\nFor rounded values was found tr_out {lut[t_index][c_index]}\n")
+        raise ValueError("[ERROR] Wrong type!")
 
-def interpolate(x, x1, x2, q11, q21):
-    return ((x2 - x)/(x2 - x1)) * q11 + ((x-x1)/(x2-x1)) * q21
+    tr1 = max([x for x in table.tr_time if x <= t_rise])
+    tr2 = min([x for x in table.tr_time if x >= t_rise])
+    cap1 = max([x for x in table.output_cap if x <= c_out])
+    cap2 = min([x for x in table.output_cap if x >= c_out])
 
-def find_interpolate(table: Table, t_rise: float, c_out: float, edge: str):
-    if edge == 'posedge':
-        lut = table.cell_rise
-    elif edge == 'negedge':
-        lut = table.cell_fall
+    tr1_idx = np.where(table.tr_time == tr1)
+    tr2_idx = np.where(table.tr_time == tr2)
+    cap1_idx = np.where(table.output_cap == cap1)
+    cap2_idx = np.where(table.output_cap == cap2)
+
+    # print(tr1_idx, tr2_idx, cap1_idx, cap2_idx)
+
+    q11 = lut[tr1_idx[0][0]][cap1_idx[0][0]]
+    q12 = lut[tr1_idx[0][0]][cap2_idx[0][0]]
+    q21 = lut[tr2_idx[0][0]][cap1_idx[0][0]]
+    q22 = lut[tr2_idx[0][0]][cap2_idx[0][0]]
+
+    if cap1 != cap2:
+        f_r1 = (cap2 - c_out) / (cap2 - cap1) * q11 + (c_out - cap1) / (cap2 - cap1) * q21
+        f_r2 = (cap2 - c_out) / (cap2 - cap1) * q12 + (c_out - cap1) / (cap2 - cap1) * q22
     else:
-        print("[ERROR] Wrong edge type!")
-        return 1
+        f_r1 = q11
+        f_r2 = q12
 
-    i = 0
-    while t_rise > table.tr_time[i]:
-        i += 1
-    t_left = table.tr_time[i - 1]
-    t_right = table.tr_time[i]
+    if tr1 != tr2:
+        delay = (tr2 - t_rise) / (tr2 - tr1) * f_r1 + (t_rise - tr1) / (tr2 - tr1) * f_r2
+    else:
+        delay = f_r1
 
-    j = 0
-    while c_out > table.output_cap[j]:
-        j += 1
-    c_left = table.output_cap[j - 1]
-    c_right = table.output_cap[j]
+    return delay
 
-    Q11, Q12, Q21, Q22 = lut[i - 1][j - 1], lut[i - 1][j], lut[i][j - 1], lut[i][j]
-    # print(Q11, Q12, Q21, Q22)
-    f1 = interpolate(c_out, c_left, c_right, Q11, Q21)
-    # print(f1)
-    f2 = interpolate(c_out, c_left, c_right, Q12, Q22)
-    # print(f2)
-    f3 = interpolate(t_rise, t_left, t_right, f1, f2)
 
-    print(f'For element {table.name} and tr_inp {t_rise}, cap_out {c_out} was interpolated tr_out {f3}\n')
+class Gate:
+    '''
+    Класс для обработки логических вентилей
+    '''
+    def __init__(self, name: str, path: str = 'lab1.lib'):
+        self.name = name
+        self.table = lib_parser(path, name)
 
-    return f3
+    def get_delay(self, edge, tr_in, c_out, method):
+        if method == 'rounded':
+            return get_value_rounded(self.table, tr_in, c_out, edge)
+        elif method == 'interpolated':
+            return get_value_interpolated(self.table, tr_in, c_out, edge)
+        else:
+            raise ValueError("[ERROR] Wrong method!")
+
+    def get_transition(self, edge, tr_in, c_out, method):
+        if method == 'rounded':
+            return get_value_rounded(self.table, tr_in, c_out, edge, 'transition')
+        elif method == 'interpolated':
+            return get_value_interpolated(self.table, tr_in, c_out, edge, 'transition')
+        else:
+            raise ValueError("[ERROR] Wrong method!")
+
+    def propagate(self, edge, tr_in, c_out, method):
+        if method == 'rounded':
+            return (get_value_rounded(self.table, tr_in, c_out, edge, 'transition') +
+                    get_value_rounded(self.table, tr_in, c_out, edge))
+        elif method == 'interpolated':
+            return (get_value_interpolated(self.table, tr_in, c_out, edge, 'transition') +
+                    get_value_interpolated(self.table, tr_in, c_out, edge))
+        else:
+            raise ValueError("[ERROR] Wrong method!")
+
 
 if __name__ == '__main__':
-    lib_table = lib_parser('lab1.lib', 'AND')
-    find_round(lib_table, t_rise=0.5, c_out=0.5, edge='posedge')
-    find_interpolate(lib_table, t_rise=0.5, c_out=0.5, edge='posedge')
+    and_gate = Gate(name='AND')
+    print(and_gate.propagate('posedge', 0.4, 0.4, 'rounded'))
